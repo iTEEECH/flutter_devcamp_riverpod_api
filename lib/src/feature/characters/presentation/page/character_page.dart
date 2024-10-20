2df891a016cd5c25d32f75d14d93c87201107d33
Florentin <florentin.bouchez@gmail.com>
2024-10-20 19:03:41 +0200

    feat: Widget list

--- lib/src/feature/characters/presentation/page/character_page.dart
+++ lib/src/feature/characters/presentation/page/character_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_devcamp_riverpod_api/src/feature/characters/application/controller/character_controller.dart';
 import 'package:flutter_devcamp_riverpod_api/src/feature/characters/domain/entity/character.dart';
-import 'package:flutter_devcamp_riverpod_api/src/feature/characters/presentation/widget/character_card.dart';
+import 'package:flutter_devcamp_riverpod_api/src/feature/characters/presentation/widget/character_list.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../widget/character_loading.dart';
@@ -18,12 +18,13 @@ class _CharactersPageState extends ConsumerState<CharactersPage> {
   bool isLoading = true;
   bool hasMore = true;
   int page = 1;
-  ScrollController controller = ScrollController();
 
   Future<void> fetchCharacters() async {
     if (!hasMore) return;
 
-    final result = await ref.read(characterControllerProvider.notifier).fetchAllCharacters(page);
+    final result = await ref
+        .read(characterControllerProvider.notifier)
+        .fetchAllCharacters(page);
 
     setState(() {
       characters.addAll(result);
@@ -41,7 +42,6 @@ class _CharactersPageState extends ConsumerState<CharactersPage> {
 
   @override
   void dispose() {
-    controller.dispose();
     super.dispose();
   }
 
@@ -52,33 +52,23 @@ class _CharactersPageState extends ConsumerState<CharactersPage> {
         title: const Text('Disney Characters'),
       ),
       body: SafeArea(
-        child: NotificationListener<ScrollNotification>(
-          onNotification: (notification) {
-            if (notification is ScrollEndNotification &&
-                notification.metrics.pixels >=
-                    notification.metrics.maxScrollExtent) {
-              fetchCharacters();
-            }
-            return false;
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24.0,
+            vertical: 12.0,
+          ),
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (notification) {
+              if (notification is ScrollEndNotification &&
+                  notification.metrics.pixels >=
+                      notification.metrics.maxScrollExtent) {
+                fetchCharacters();
+              }
+              return false;
+            },
             child: isLoading
                 ? const CharacterLoading()
-                : ListView.builder(
-              controller: controller,
-              itemCount: characters.length,
-              physics: const ClampingScrollPhysics(),
-              itemBuilder: (context, int index) {
-                final character = characters[index];
-                return CharacterCard(
-                  key: ValueKey(index),
-                  name: character.name,
-                  imageUrl: character.imageUrl,
-                  films: character.films,
-                );
-              },
-            ),
+                : CharactersList(characters: characters),
           ),
         ),
       ),
